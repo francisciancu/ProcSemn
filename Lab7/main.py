eb0f5c5a5208af5bda15e7f210da5a4e10b6c742
Francisc Andrei Sebastian Iancu <andreiiancu14@gmail.com>
2023-11-30 14:59:22 +0200

    Lab7

--- Lab7/main.py
+++ Lab7/main.py
@@ -110,5 +110,89 @@ def ex1():
     return
 
 
+def ex2(SNR=5):
+    def calcSNR(img: np.ndarray, img_compressed: np.ndarray) -> float:
+        snr = 0
+        entries = 0
+
+        for i in range(len(img)):
+            for j in range(len(img[0])):
+                currentPixImg = img[i, j]
+                currentPixImgCompr = img_compressed[i, j] if img_compressed[i, j] != 0 else 0.1
+                divided = currentPixImg / currentPixImgCompr
+                snr += divided
+                entries += 1
+        return abs(snr / entries)
+
+    cut_off = 160
+    X = misc.face(gray=True)
+    while True:
+        print(cut_off)
+        Y = np.fft.fft2(X)
+
+        freq_db = 20 * np.log10(abs(Y))
+        m_freq = freq_db.max()
+        Y[freq_db > (m_freq - cut_off)] = 0
+
+        X_cut = np.real(np.fft.ifft2(Y))
+        SNRcalc = abs(calcSNR(X, X_cut))
+        print((calcSNR(X, X_cut)))
+
+        if SNRcalc < SNR:
+            break
+        elif cut_off < 0:
+            X_cut = X
+            break
+        else:
+            cut_off -= 20
+            if cut_off == 60:
+                cut_off -= 1
+
+    plt.subplot(121).imshow(X, cmap=plt.cm.gray)
+    plt.title('Original')
+
+    plt.subplot(122).imshow(X_cut, cmap=plt.cm.gray)
+    plt.title('Noisy')
+    plt.show()
+    return
+
+
+def signaltonoise(a, axis=0, ddof=0):
+    a = np.asanyarray(a)
+    m = a.mean(axis)
+    sd = a.std(axis=axis, ddof=ddof)
+    return np.where(sd == 0, 0, m / sd)
+#https://stackoverflow.com/questions/25524192/how-to-get-the-signal-to-noise-ratio-from-an-image-in-python
+
+
+def ex3():
+    X = misc.face(gray=True)
+    noise = np.random.normal(0, 0.1, X.shape)
+    noisy_X = X + noise
+
+    plt.subplot(121).imshow(X, cmap=plt.cm.gray)
+    plt.title('Original')
+    plt.subplot(122).imshow(noisy_X, cmap=plt.cm.gray)
+    plt.title('Noisy')
+    plt.show()
+    original_snr = signaltonoise(X, axis=None)
+    print('Original SNR:', original_snr)
+
+    snr_noise = signaltonoise(noisy_X, axis=None)
+    print('SNR of noisy:', snr_noise)
+
+    estimated_noise = noisy_X - X
+    new_X = noisy_X - estimated_noise
+    snr_after = signaltonoise(new_X, axis=None)
+    print('Final SNR:', snr_after)
+    plt.subplot(131).imshow(noisy_X, cmap=plt.cm.gray)
+    plt.title('Noisy')
+    plt.subplot(132).imshow(new_X, cmap=plt.cm.gray)
+    plt.title('New Original')
+    plt.subplot(133).imshow(X, cmap=plt.cm.gray)
+    plt.title('Original')
+    plt.show()
+
+
 if __name__ == "__main__":
-    ex1()
+    ex3()
